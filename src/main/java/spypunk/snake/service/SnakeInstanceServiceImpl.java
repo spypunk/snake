ba4c0a471bbf1fe0fd1d726798c3e3ee705a7a9f
spypunk <adrien.spanier@gmail.com>
2016-09-12 19:27:32 +0200

    Update start position

--- src/main/java/spypunk/snake/service/SnakeInstanceServiceImpl.java
+++ src/main/java/spypunk/snake/service/SnakeInstanceServiceImpl.java
@@ -38,14 +38,14 @@ public class SnakeInstanceServiceImpl implements SnakeInstanceService {
     public void create(final Snake snake) {
         final List<Point> snakeParts = Lists.newArrayList();
 
-        Point headLocation = new Point(SnakeConstants.WIDTH / 2, SnakeConstants.HEIGHT / 2);
+        int x = SnakeConstants.WIDTH / 2;
 
-        snakeParts.add(headLocation);
-        snakeParts.add(new Point(headLocation.x + 1, headLocation.y));
-        snakeParts.add(new Point(headLocation.x + 2, headLocation.y));
+        snakeParts.add(new Point(x, 2));
+        snakeParts.add(new Point(x, 1));
+        snakeParts.add(new Point(x, 0));
 
         final SnakeInstance snakeInstance = SnakeInstance.Builder.instance()
-                .setSpeed(SnakeConstants.DEFAULT_SPEED).setState(State.RUNNING).setSnakeDirection(Direction.UP)
+                .setSpeed(SnakeConstants.DEFAULT_SPEED).setState(State.RUNNING).setSnakeDirection(Direction.DOWN)
                 .setSnakeParts(snakeParts)
                 .build();
 
